ca2f7609c48609c379c606a052d04396f5d636b1
Christina Vicente <christina.vicente@hcl.com>
2021-02-15 17:45:58 -0500

    created basic login controller

--- src/main/java/com/example/TaskManager/Controllers/LoginController.java
+++ src/main/java/com/example/TaskManager/Controllers/LoginController.java
@@ -1,4 +1,40 @@
 package com.example.TaskManager.Controllers;
 
+import com.example.TaskManager.entities.User;
+import com.example.TaskManager.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class LoginController {
+
+    @Autowired
+    UserService userService;
+
+    public String getCredentials(@RequestParam String name, @RequestParam String password ){
+
+
+        return "login";
+    }
+
+    public String displayLoggedIn(){
+
+        return "success";
+    }
+
+    public String displayError(){
+
+        return "error";
+    }
+
+    private boolean authenticate(String name, String password){
+        for(User user:userService.getAllUsers()){
+                if((user.getName().equals(name))&&(user.getPassword().equals(password))){
+                    return true;
+                }
+            }
+        return false;
+    }
+
 }
