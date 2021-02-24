42c6a8c05e0f54bbbf5c5c731fe48eb0a1feb320
christinavicente <76453287+christinavicente@users.noreply.github.com>
2021-02-23 22:47:13 -0500

    Update TaskManagerApplication.java

--- src/main/java/com/example/TaskManager/TaskManagerApplication.java
+++ src/main/java/com/example/TaskManager/TaskManagerApplication.java
@@ -3,7 +3,10 @@ package com.example.TaskManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan({ "com.example.Taskmaster" })
+@PropertySources(value = { @PropertySource("classpath:/application.properties") })
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
